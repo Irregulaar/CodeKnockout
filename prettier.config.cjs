ee95e3915e432d6888a1169d5b426ffb4116a9e0
Concurr3ncia <kevinsd872@gmail.com>
2025-03-27 17:28:02 -0500

    Initialize React application with Vite, Tailwind CSS, and ESLint configuration

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+      "prettier-plugin-tailwindcss",
+      "prettier-plugin-classnames"
+    ],
+    printWidth: 150,
+    endingPosition: "absolute-with-indent",
+    experimentalOptimization: true,
+  };
+  
