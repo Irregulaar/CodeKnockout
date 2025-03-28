d56e7982b73db61ee7bc2e9a37937160c4246b46
Concurr3ncia <kevinsd872@gmail.com>
2025-03-27 21:42:45 -0500

    Add Pyodide and static copy plugin; update meta tags in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  optimizeDeps: { exclude: ["pyodide"] },
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: join(dirname(fileURLToPath(import.meta.url)), "node_modules/pyodide"),
+          dest: "assets",
+        },
+      ],
+    }),
+  ],
 });
