5a90b9262a4657ee53135fa7ac87d5ccdf10e8ff
Concurr3ncia <kevinsd872@gmail.com>
2025-03-27 21:43:15 -0500

    Refactor Header and Layout components for improved styling and layout consistency

--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex flex-row w-screen bg-[#121212] relative h-15 items-center text-white border-[#ffffff60] border-b-1">
-      <img className="h-full cursor-pointer pl-10 pr-10" src="/logo.png"></img>
+      <img className="h-full cursor-pointer pl-10" src="/logo.png"></img>
       <button
         className="cursor-pointer text-[12px] opacity-60 hover:opacity-100 transition-all font-light tracking-wider w-fit pr-3 pl-3 h-[80%]
           duration-300"
@@ -29,10 +29,13 @@ function Header() {
           Sign up
         </button>
         <button
-          className="cursor-pointer text-[12px] opacity-60 hover:opacity-100 transition-all font-light tracking-wider border-1 w-20 h-full
-            text-[#ff8d41] border-[#ff8d41] rounded duration-300"
+          className="cursor-pointer text-[12px] opacity-60 hover:opacity-100 transition-all font-light tracking-wider w-20 h-full bg-gradient-to-b
+            from-[#ff58a6] to-[#ffd23d] rounded duration-300 flex items-center p-[1.5px] justify-center"
         >
-          Join
+          <div className="w-full h-full flex items-center justify-center bg-[#121212] rounded"></div>
+          <h1 className="absolute font-semibold bg-clip-text bg-gradient-to-b from-[#ff58a6] to-[#ffd23d] w-fit h-fit text-transparent text-center">
+            Join
+          </h1>
         </button>
       </div>
     </header>
--- src/components/landing/Layout.jsx
+++ src/components/landing/Layout.jsx
@@ -2,10 +2,10 @@ function Layout() {
   return (
     <section className="w-full h-full flex flex-col items-center text-white">
       <div className="bg-[#121212] fixed h-full w-full -z-10"></div>
-      <img className="bg-[#121212] fixed opacity-10 -z-9" src="/layout.png"></img>
+      <img className="bg-[#121212] w-full h-full fixed opacity-30 -z-9" src="/layout.png"></img>
       <div className="mb-5">
         <h1
-          className="text-4xl font-semibold bg-clip-text bg-gradient-to-b from-[#E7257F] to-[#F1BA02] w-fit h-fit text-transparent mt-20 line-clamp-3
+          className="text-4xl font-semibold bg-clip-text bg-gradient-to-b from-[#ff58a6] to-[#ffd23d] w-fit h-fit text-transparent mt-20 line-clamp-3
             balance text-center"
         >
           ¡DUELOS DE PROGRAMACIÓN{" "}
@@ -19,7 +19,7 @@ function Layout() {
       </h1>
 
       <button
-        className="cursor-pointer transition-all bg-gradient-to-b from-[#E7257F] to-[#F1BA02] tracking-wider p-1 rounded duration-300 text-[#251b06]
+        className="cursor-pointer transition-all bg-gradient-to-b from-[#ff58a6] to-[#ffd23d] tracking-wider p-1 rounded duration-300 text-[#251b06]
           font-medium text-[15px] w-25 h-10 hover:opacity-60"
       >
         Empieza
