5df021e156ea3de2838377e73e39d1e427be0fa2
Safestt <zakbass280@gmail.com>
2025-04-03 18:35:11 -0500

    Add Matchcompletion router to the main router configuration

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,4 +1,5 @@
 from .auth_routes import router as auth_router
 from .match_routes import router as match_routers
+from .Matchcompletion import router as Matchcompletion
 
-all_routers = [auth_router, match_routers]
+all_routers = [auth_router, match_routers,Matchcompletion]
