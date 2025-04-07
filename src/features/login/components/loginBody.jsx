11be357653bca4f97ed6b4fd53209b9ecb98bdba
Concurr3ncia <kevinsd872@gmail.com>
2025-04-07 03:03:04 -0500

    feat(login): add email validation, login, and loading states
    feat(register): implement registration form with validation and loading states
    
    This commit introduces new features for the login and registration components. The login component now includes email validation, login functionality, and loading states. The registration component has been updated to handle user registration with input validation and loading states. Both components now provide better user feedback and error handling.

--- src/features/login/components/loginBody.jsx
+++ src/features/login/components/loginBody.jsx
@@ -2,16 +2,97 @@ import { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 
 function LoginBody() {
-  const [stage, setStage] = useState(2);
+  const [stage, setStage] = useState(1);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleStageChange = () => {
     setStage(stage === 1 ? 2 : 1);
+    setPassword(""); // Limpiar password al cambiar de stage
+  };
+
+  const handleLogin = async () => {
+    setLoading(true);
+    setPasswordError(null);
+
+    if (password === "") {
+      setPasswordError("Password is required");
+      setLoading(false);
+      return;
+    }
+
+    const response = await fetch("https://safe-github-io.onrender.com/auth/login", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      setPasswordError("Invalid credentials");
+      setLoading(false);
+      throw new Error("Error al iniciar sesión");
+    }
+
+    const data = await response.json();
+    localStorage.setItem("token", data.access_token);
+    localStorage.setItem("user", JSON.stringify(data.datauser));
+    window.location.href = "/dashboard";
+  };
+
+  const emailContinue = async () => {
+    setLoading(true);
+    setEmailError(null);
+    setPasswordError(null);
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email === "") {
+      setEmailError("Email is required");
+      setLoading(false);
+      return;
+    } else if (!emailRegex.test(email)) {
+      setEmailError("Invalid email format");
+      setLoading(false);
+      return;
+    }
+
+    const response = await fetch(`https://safe-github-io.onrender.com/auth/validate-email?email=${email}`, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      setLoading(false);
+      throw new Error("Login error");
+    }
+
+    const data = await response.json();
+
+    if (data.message === true) {
+      setLoading(false);
+      setStage(2);
+    } else {
+      setEmailError("Email is not registered");
+      setLoading(false);
+      return;
+    }
   };
 
   return (
     <>
       {stage === 1 ? (
-        <div className="text-regular bg-dark mt-20 mb-15 flex h-fit w-[90%] flex-col items-center justify-start rounded-xl p-10 md:w-100">
+        <div
+          className="text-regular bg-dark mt-20 mb-15 flex h-fit w-[90%] flex-col items-center justify-start rounded-xl p-10
+            md:w-100"
+        >
           <h1 className="text-[30px] font-semibold">Welcome back!</h1>
           <p className="text-[12px]">Please log in to continue.</p>
 
@@ -23,29 +104,49 @@ function LoginBody() {
           </p>
           <input
             type="text"
-            className="bg-regular border-regular bg-dark mt-5 flex h-10 w-full flex-row items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none"
+            value={email}
+            disabled={loading}
+            onChange={(e) => setEmail(e.target.value)}
+            className={`${emailError ? "border-red-500" : "border-regular"} bg-regular bg-dark mt-5 flex h-10 w-full flex-row
+              items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none `}
             placeholder="Email"
           />
-          <button className="bg-light mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
-            Continue
+          {emailError && <p className="text-red-500 text-[12px] self-start">{emailError}</p>}
+          <button
+            onClick={emailContinue}
+            disabled={loading}
+            className="bg-light mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center
+              hover:underline"
+          >
+            {loading ? "Loading..." : "Continue"}
           </button>
 
           <div className="mt-5 mb-4 flex h-px w-full items-center justify-center bg-white opacity-50">
             <p className="bg-dark p-1 text-center text-[12px]">OR</p>
           </div>
 
-          <button className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
+          <button
+            className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center
+              hover:underline"
+          >
             Continue with Google
           </button>
-          <button className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
+          <button
+            className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center
+              hover:underline"
+          >
             Continue with GitHub
           </button>
         </div>
       ) : (
-        <div className="text-regular bg-dark mt-20 mb-20 flex h-fit w-[90%] flex-col items-center justify-start rounded-xl p-10 md:w-100">
+        <div
+          className="text-regular bg-dark mt-20 mb-20 flex h-fit w-[90%] flex-col items-center justify-start rounded-xl p-10
+            md:w-100"
+        >
           <div
             onClick={handleStageChange}
-            className="bg-dark hover:bg-light mb-5 flex h-fit w-fit cursor-pointer flex-row items-center justify-center gap-2 self-start rounded p-2 transition-colors duration-200"
+            className="bg-dark hover:bg-light mb-5 flex h-fit w-fit cursor-pointer flex-row items-center justify-center gap-2
+              self-start rounded p-2 transition-colors duration-200"
           >
             <FaArrowLeft size={15} className="cursor-pointer" />
             <p>Back</p>
@@ -55,11 +156,21 @@ function LoginBody() {
 
           <input
             type="password"
-            className={`bg-regular border-regular bg-dark mt-3 flex h-10 w-full flex-row items-center justify-center rounded-lg border p-2 text-start text-[12px] outline-none`}
+            value={password}
+            disabled={loading}
+            onChange={(e) => setPassword(e.target.value)}
+            className={`${passwordError ? "border-red-500" : "border-regular"} bg-regular bg-dark mt-5 flex h-10 w-full flex-row
+              items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none `}
             placeholder="Password"
           />
-          <button className="bg-light mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
-            Log in
+          {passwordError && <p className="text-red-500 text-[12px] self-start">{passwordError}</p>}
+          <button
+            onClick={handleLogin}
+            disabled={loading}
+            className="bg-light mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center
+              hover:underline"
+          >
+            {loading ? "Loading..." : "Log in"}
           </button>
 
           <p className="mt-3 flex gap-2 text-[12px]">
--- src/features/register/components/registerBody.jsx
+++ src/features/register/components/registerBody.jsx
@@ -1,88 +1,160 @@
 import { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 
-function LoginBody() {
-  const [stage, setStage] = useState(1);
+function RegisterBody() {
+  // Changed from LoginBody to RegisterBody
+  const [loading, setLoading] = useState(false);
 
-  const handleStageChange = () => {
-    setStage(stage === 1 ? 2 : 1);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+
+  const [errorEmail, setErrorEmail] = useState(null);
+  const [errorPassword, setErrorPassword] = useState(null);
+
+  const [errorUsername, setErrorUsername] = useState(null);
+
+  const handleRegisterSubmit = async (e) => {
+    setLoading(true);
+    setError(null);
+    setErrorEmail(null);
+    setErrorPassword(null);
+    setErrorUsername(null);
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email === "") {
+      setErrorEmail("Email address is required");
+      setLoading(false);
+      return;
+    } else if (!emailRegex.test(email)) {
+      setErrorEmail("Please enter a valid email address");
+      setLoading(false);
+      return;
+    }
+
+    if (username === "") {
+      setErrorUsername("Username is required");
+      setLoading(false);
+      return;
+    }
+
+    if (password === "") {
+      setErrorPassword("Password is required");
+      setLoading(false);
+      return;
+    } else if (confirmPassword === "") {
+      setErrorPassword("Please confirm your password");
+      setLoading(false);
+      return;
+    } else if (password !== confirmPassword) {
+      setErrorPassword("Passwords do not match");
+      setLoading(false);
+      return;
+    }
+
+    const response = await fetch("https://safe-github-io.onrender.com/auth/register", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password, username }),
+    });
+
+    if (!response.ok) {
+      setLoading(false);
+      throw new Error("Registration failed. Please try again.");
+    }
+
+    setLoading(false);
+    window.location.href = "/login";
   };
 
   return (
-    <>
-      {stage === 1 ? (
-        <div className="text-regular bg-dark mt-20 mb-15 flex h-fit w-[90%] flex-col items-center justify-start rounded-xl p-10 md:w-100">
-          <h1 className="text-[30px] font-semibold">Sign up</h1>
+    <div
+      className="text-regular bg-dark mt-20 mb-15 flex h-fit w-[90%] flex-col items-center justify-start rounded-xl pt-5 p-10
+        md:w-100"
+    >
+      <h1 className="text-[30px] font-semibold">Sign up</h1>
 
-          <p className="mt-1 flex gap-2 text-[12px]">
-            You have a account?{" "}
-            <a href="/login" className="cursor-pointer font-bold hover:underline">
-              Log in
-            </a>
-          </p>
-          <input
-            type="text"
-            className="bg-regular border-regular bg-dark mt-2 flex h-10 w-full flex-row items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none"
-            placeholder="Username"
-          />
-          <input
-            type="text"
-            className="bg-regular border-regular bg-dark mt-2 flex h-10 w-full flex-row items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none"
-            placeholder="Email"
-          />
-          <input
-            type="password"
-            className="bg-regular border-regular bg-dark mt-2 flex h-10 w-full flex-row items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none"
-            placeholder="Password"
-          />
-          <input
-            type="password"
-            className="bg-regular border-regular bg-dark mt-2 flex h-10 w-full flex-row items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none"
-            placeholder="Confirm password"
-          />
-          <button className="bg-light mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
-            Continue
-          </button>
+      <p className="mt-1 flex gap-2 text-[12px]">
+        You have a account?{" "}
+        <a href="/login" className="cursor-pointer font-bold hover:underline">
+          Log in
+        </a>
+      </p>
+      <input
+        type="text"
+        value={username}
+        disabled={loading}
+        onChange={(e) => setUsername(e.target.value)}
+        className={`${errorUsername ? "border-red-500" : "border-regular"} bg-regular bg-dark mt-2 flex h-10 w-full flex-row
+          items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none`}
+        placeholder="Username"
+      />
+      {errorUsername && <p className="text-red-500 text-[12px] self-start">{errorUsername}</p>}
 
-          <div className="mt-5 mb-4 flex h-px w-full items-center justify-center bg-white opacity-50">
-            <p className="bg-dark p-1 text-center text-[12px]">OR</p>
-          </div>
+      <input
+        type="text"
+        value={email}
+        disabled={loading}
+        onChange={(e) => setEmail(e.target.value)}
+        className={`${errorEmail ? "border-red-500" : "border-regular"} bg-regular bg-dark mt-2 flex h-10 w-full flex-row
+          items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none`}
+        placeholder="Email"
+      />
+      {errorEmail && <p className="text-red-500 text-[12px] self-start">{errorEmail}</p>}
 
-          <button className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
-            Continue with Google
-          </button>
-          <button className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
-            Continue with GitHub
-          </button>
-        </div>
-      ) : (
-        <div className="text-regular bg-dark mt-20 mb-20 flex h-fit w-[90%] flex-col items-center justify-start rounded-xl p-10 md:w-100">
-          <div
-            onClick={handleStageChange}
-            className="bg-dark hover:bg-light mb-5 flex h-fit w-fit cursor-pointer flex-row items-center justify-center gap-2 self-start rounded p-2 transition-colors duration-200"
-          >
-            <FaArrowLeft size={15} className="cursor-pointer" />
-            <p>Back</p>
-          </div>
-          <h1 className="text-[30px] font-semibold">Welcome back!</h1>
-          <p className="text-[12px]">Please log in to continue.</p>
+      <input
+        type="password"
+        value={password}
+        disabled={loading}
+        onChange={(e) => setPassword(e.target.value)}
+        className={`${errorPassword ? "border-red-500" : "border-regular"} bg-regular bg-dark mt-2 flex h-10 w-full flex-row
+          items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none`}
+        placeholder="Password"
+      />
 
-          <input
-            type="password"
-            className={`bg-regular border-regular bg-dark mt-3 flex h-10 w-full flex-row items-center justify-center rounded-lg border p-2 text-start text-[12px] outline-none`}
-            placeholder="Password"
-          />
-          <button className="bg-light mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center hover:underline">
-            Log in
-          </button>
+      <input
+        type="password"
+        value={confirmPassword}
+        disabled={loading}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        className={`${errorPassword ? "border-red-500" : "border-regular"} bg-regular bg-dark mt-2 flex h-10 w-full flex-row
+          items-center justify-center rounded-lg border-1 p-2 text-start text-[12px] outline-none`}
+        placeholder="Confirm password"
+      />
+      {errorPassword && <p className="text-red-500 text-[12px] self-start">{errorPassword}</p>}
 
-          <p className="mt-3 flex gap-2 text-[12px]">
-            Forgot your password? <span className="cursor-pointer font-bold hover:underline">Reset password</span>
-          </p>
-        </div>
-      )}
-    </>
+      <button
+        onClick={handleRegisterSubmit}
+        disabled={loading}
+        className="bg-light mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center
+          hover:underline"
+      >
+        {loading ? "Loading..." : "Continue"}
+      </button>
+
+      <div className="mt-5 mb-4 flex h-px w-full items-center justify-center bg-white opacity-50">
+        <p className="bg-dark p-1 text-center text-[12px]">OR</p>
+      </div>
+
+      <button
+        className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center
+          hover:underline"
+      >
+        Continue with Google
+      </button>
+      <button
+        className="bg-regular mt-3 flex h-10 w-full cursor-pointer items-center justify-center rounded-lg text-center
+          hover:underline"
+      >
+        Continue with GitHub
+      </button>
+    </div>
   );
 }
 
-export default LoginBody;
+export default RegisterBody; // Changed from LoginBody to RegisterBody
